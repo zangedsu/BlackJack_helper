8c7ec022b51854480205a5a61f8070c6bbb6c608
Vitaliy <zangedsu@gmail.com>
2023-05-08 19:31:28 +0300

    Добавьте файлы проекта.

---
+++ BlackJeck_helper/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJeck_helper.Models;
+    internal class Card
+    {
+    }
+
