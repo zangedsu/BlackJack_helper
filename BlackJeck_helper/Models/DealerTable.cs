e036a8731c0537da4aefc1fa0e3da2db2acd134c
Vitaliy <zangedsu@gmail.com>
2023-05-15 18:07:37 +0300

    Привязал отображение количества карт к их колодам

--- BlackJeck_helper/Models/DealerTable.cs
+++ BlackJeck_helper/Models/DealerTable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlackJeck_helper.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace BlackJack_helper.Models;
 
     public class DealerTable
     {
+    private Deck _deck = new Deck(0);
+    public Deck DealerDeck { get { return _deck; } }
+
+    public DealerTable()
+    {
+        _deck = new Deck(0);
     }
+
+}
 
--- BlackJeck_helper/Models/UserTable.cs
+++ BlackJeck_helper/Models/UserTable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlackJeck_helper.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace BlackJack_helper.Models;
 
     public class UserTable
     {
+    private Deck _deck = new Deck(0);
+    public Deck UserDeck { get { return _deck; } }
+
+    public UserTable()
+    {
+        _deck = new Deck(0);
     }
+}
 
